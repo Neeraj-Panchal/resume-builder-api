899aaa97c30695a9a88decfba50df28bc0d695a0
Neeraj Panchal <neerajpanchal098@gmail.com>
2026-03-13 04:23:44 +0530

    Updated MongoConfig for dynamic cloud connection

--- src/main/java/com/neeraj/resumebuilderapi/config/MongoConfig.java
+++ src/main/java/com/neeraj/resumebuilderapi/config/MongoConfig.java
@@ -2,7 +2,7 @@ package com.neeraj.resumebuilderapi.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -13,15 +13,19 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = "com.neeraj.resumebuilderapi.repository")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+
+    @Value("${spring.data.mongodb.uri:mongodb://localhost:27017/resumebuilder}")
+    private String mongoUri;
+
     @Override
     protected String getDatabaseName() {
-        // Yeh line default 'test' database ko block kar degi
+
         return "resumebuilder";
     }
 
     @Override
     public MongoClient mongoClient() {
-        // Aapka local connection setup
-        return MongoClients.create("mongodb://localhost:27017");
+
+        return MongoClients.create(mongoUri);
     }
 }
